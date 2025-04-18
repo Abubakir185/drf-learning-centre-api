30b9f6cf75fc9ccf38d6ae8b3b9c1e2312e342c8
Abubakir185 <abubakrbohodirov185@gmail.com>
2025-04-18 19:27:10 +0500

    Update settings.py

--- online_centre/config/settings.py
+++ online_centre/config/settings.py
@@ -104,16 +104,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "online_course",
-        "USER": "postgres",
-        "PASSWORD": "abubakir",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
